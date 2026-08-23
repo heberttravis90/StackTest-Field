c60b471f0e957efecbecc1926af52a7b47b47e56
heberttravis90 <heberttravis90@gmail.com>
2026-08-23 14:20:12 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="stacktestpro-v07-organized";
-const ASSETS=["./","index.html","style.css","app.js","manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
